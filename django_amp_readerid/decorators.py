bf8f62d7d00ffba9d055bf693a4760a21ffbf8e7
Aníbal Pacheco <apacheco.uy@gmail.com>
2023-04-29 17:22:59 -0300

    TODO upd

--- django_amp_readerid/decorators.py
+++ django_amp_readerid/decorators.py
@@ -1,10 +1,10 @@
-from django.contrib.auth.signals import user_logged_in
-
-
-def amp_readerid_assoc(sender, user, request, **kwargs):
-    print("DEBUG: amp_readerid_assoc signal request: %s" % request)##
-
-
-# TODO: we should use a decorator for the amp-login view, update readme (no signal or middleware, decorator!)
-#       Also for readme: implement the amp-login view is site dev's responsibility
-user_logged_in.connect(do_stuff)
+"""
+TODO:
+We should implement a decorator here for the amp-login view, update readme (no signal or middleware, decorator!)
+Also for readme: implement the amp-login view is site dev's responsibility
+Things to take care in the decorator:
+ - the parameter name of the "readerID" included in the GET request
+ - the login view can already set some variables to redirect to a "next" or "return" URL, we must not change anything
+   of this behaviour, unless the reader ID is received, this indicates that this is a login from AMP and the request
+   also should came with a "return" parameter to redirect
+"""
