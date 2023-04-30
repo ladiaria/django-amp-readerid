80f3294dcc4b561a3c714621a38a0d823c34cf9e
Aníbal Pacheco <apacheco.uy@gmail.com>
2023-04-30 02:41:50 -0300

    upd

--- django_amp_readerid/decorators.py
+++ django_amp_readerid/decorators.py
@@ -4,7 +4,6 @@ We should implement a decorator here for the amp-login view, things to take care
  - the parameter name of the "readerID" included in the GET request
  - the login view can already set some variables to redirect to a "next" or "return" URL, we must not change anything
    of this behaviour, unless the reader ID is received, this indicates that this is a login from AMP and the request
-   also should came with a "return" parameter to redirect (an "url" param may also be received, for the page where the
-   login was clicked, this may be used instead of the "return" in dev time, because dev page will not be cached in
-   google)
+   also should came with a "return" parameter to redirect (in dev time the param value may need rerwited, because dev
+   page will not be cached in google)
 """
