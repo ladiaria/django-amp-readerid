7ee211baa11198b2e15781beb4a92daa6968b594
Aníbal Pacheco <apacheco.uy@gmail.com>
2023-05-02 14:02:13 -0300

    wip

--- django_amp_readerid/decorators.py
+++ django_amp_readerid/decorators.py
@@ -1,9 +1,20 @@
-"""
-TODO:
-We should implement a decorator here for the amp-login view, things to take care in the decorator:
- - the parameter name of the "readerID" included in the GET request
- - the login view can already set some variables to redirect to a "next" or "return" URL, we must not change anything
-   of this behaviour, unless the reader ID is received, this indicates that this is a login from AMP and the request
-   also should came with a "return" parameter to redirect (in dev time the param value may need rerwited, because dev
-   page will not be cached in google)
-"""
+from django.conf import settings
+
+
+def readerid_assoc(login_view):
+    """
+    TODO: decorator for the amp-login view, things to take care in the decorator:
+    - if form is not valid in post phase
+    - the parameter name of the "readerID" included in the GET request (should be an app setting)
+    - the login view can already set some variables to redirect to a "next" or "return" URL, we must not change anything
+      of this behaviour, unless the reader ID is received, this indicates that this is a login from AMP and the request
+      also should came with a "return" parameter to redirect (in dev time the param value may need rerwited, because dev
+      page will not be cached in google)
+    """
+
+    def assoc(request, *args, **kwargs):
+        if settings.DEBUG:
+            print(request.GET)##
+        return login_view(request, *args, **kwargs)
+
+    return assoc
